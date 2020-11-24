cfc769288a6a32bac6c8486b9921ce827314d709
Dominykas Sipelis <dsipel2@uic.edu>
2020-11-23 22:17:11 -0600

    added large interactable objects

---
+++ Assets/Scripts/PlayPinball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PlayPinball : MonoBehaviour
+{
+    public VRTK_InteractableObject pinball;
+    public AudioSource sound;
+
+    protected virtual void OnEnable() {
+        pinball.InteractableObjectUsed += InteractableObjectUsed;
+    }
+
+    void InteractableObjectUsed(object sender, InteractableObjectEventArgs e) {
+        sound.PlayOneShot(sound.clip);
+    }
+}
---
+++ Assets/Scripts/sodaSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class sodaSpawn : MonoBehaviour
+{
+    public VRTK_InteractableObject machine;
+    public GameObject sodaPrefab;
+
+    protected virtual void OnEnable() {
+        machine.InteractableObjectUsed += InteractableObjectUsed;
+    }
+
+    void InteractableObjectUsed(object sender, InteractableObjectEventArgs e) {
+        Debug.Log("spawn soda");
+        Instantiate(sodaPrefab);
+    }
+}
